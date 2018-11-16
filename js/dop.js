b81a614a52e46f2c2ce55e946ff5d09ad763fc40
Vladimir ASU <vladimir_t1@mail.ru>
2018-11-16 12:48:57 +0500

    первый коммит

---
+++ js/dop.js
@@ -0,0 +1,17 @@
+var num = String(33721);
+
+var a = (num.charAt(0));
+var b = (num.charAt(1));
+var c = (num.charAt(2));
+var d = (num.charAt(3));
+var e = (num.charAt(4));
+var multi = (a * b * c * d * e);
+
+var exp = 3;
+var deg = String(multi**exp);
+
+var y = (deg.charAt(0));
+var x = (deg.charAt(1));
+
+console.log(y + x)
+
